78cc80acb861cd70add32a2a321e3b70ae7df757
Akash kumar <akashkumar972177@gmail.com>
2026-04-15 02:15:00 +0530

    slidingwindow pattern max consecution one count program added

---
+++ src/com/java/dsa/slidingWindow/FindMaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+package com.java.dsa.slidingWindow;
+
+class FindMaxConsecutiveOnes {
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int cnt=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                cnt++;
+                max=Math.max(cnt,max);
+            }
+            else{
+                cnt=0;
+                max=Math.max(cnt,max);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int nums[]={1,1,0,1,1,1};
+        int maxConsecutiveOnes = findMaxConsecutiveOnes(nums);
+        System.out.println("maximum consuecutive one is :"+maxConsecutiveOnes);
+    }
+}
