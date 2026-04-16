fe9df7b8ccea1677c9d3cdd01d19e045c573d1d0
Akash kumar <akashkumar972177@gmail.com>
2026-04-16 17:43:51 +0530

    maximumsubarray average of size k added

---
+++ src/com/java/dsa/slidingWindow/MaxSubarrayAverageOfSizeK.java
@@ -0,0 +1,45 @@
+package com.java.dsa.slidingWindow;
+
+public class MaxSubarrayAverageOfSizeK {
+    public static double usingBruteforce(int arr[],int k){
+
+        double maxSum=Integer.MIN_VALUE;
+        int n=arr.length;
+        for(int i=0;i<=n-k;i++){
+            double currSum=0;
+            for(int j=i;j<k+i;j++){
+                currSum+=arr[j];
+            }
+            maxSum=Math.max(currSum,maxSum);
+        }
+        return maxSum/k;
+    }
+
+    public static double usingOptimize(int arr[],int k){
+        int n=arr.length;
+        double currSum=0;
+        double maxSum=0;
+        for(int i=0;i<k;i++){
+            currSum+=arr[i];
+        }
+
+        maxSum=currSum;
+        for(int i=k;i<n;i++){
+            currSum+=arr[i]-arr[i-k];
+            maxSum=Math.max(currSum,maxSum);
+        }
+        return maxSum/k;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,12,-5,-6,50,3};
+        int k=4;
+        System.out.println("maximum subarray sum of size "+k);
+        double maxavgofsizek_1 = usingBruteforce(arr, k);
+        System.out.println(maxavgofsizek_1);
+
+        double maxavgofsizek_2 = usingOptimize(arr, k);
+        System.out.println(maxavgofsizek_2);
+
+
+    }
+}
